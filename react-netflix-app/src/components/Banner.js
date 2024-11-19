2f813d2097baf531df9b4100af3d4419dff4db8a
mskwon02 <121784261+mskwon02@users.noreply.github.com>
2024-11-20 03:56:44 +0900

    배너완성_play클릭시 화면에 동영상 불러오기

--- react-netflix-app/src/components/Banner.js
+++ react-netflix-app/src/components/Banner.js
@@ -4,6 +4,7 @@ import requests_defined from "../api/requests_defined";
 
 function Banner() {
   const [bannerMovie, setbannerMovie] = useState([]);
+  const [isClicked, setisClicked] = useState(false);
   useEffect(() => {
     fetchData();
     return () => {};
@@ -18,42 +19,58 @@ function Banner() {
         .id;
     const { data: banner_movie_detail } = await axios_instance.get(
       `movie/${banner_movie_id}`,
-      { params: { append_to_response: "video" } }
+      { params: { append_to_response: "videos" } }
     );
     setbannerMovie(banner_movie_detail);
   };
-
-  return (
-    <header
-      style={{
-        backgroundImage: `url('https://image.tmdb.org/t/p/original/${bannerMovie?.backdrop_path}')`,
-        backgroundPosition: "top center",
-        backgroundSize: "cover",
-      }}
-      className="h-[650px] object-contain flex flex-col justify-center"
-    >
-      <div className="p-16 flex flex-col justify-between h-2/3 mt-16">
-        <h1 className="text-white font-bold text-6xl">
-          {bannerMovie?.title ||
-            bannerMovie?.name ||
-            bannerMovie?.original_name}
-        </h1>
-        <div>
-          <button className="mr-6 bg-white px-6 py-2 rounded-md text-2xl font-semibold">
-            play
-          </button>
-          <button className=" bg-gray-400 bg-opacity-55 px-4 py-2 rounded-md text-2xl font-semibold text-white">
-            More Information
-          </button>
+  if (!isClicked) {
+    return (
+      <header
+        style={{
+          backgroundImage: `url('https://image.tmdb.org/t/p/original/${bannerMovie?.backdrop_path}')`,
+          backgroundPosition: "top center",
+          backgroundSize: "cover",
+        }}
+        className="h-[650px] object-contain flex flex-col justify-center"
+      >
+        <div className="p-16 flex flex-col justify-between h-2/3 mt-16">
+          <h1 className="text-white font-bold text-6xl">
+            {bannerMovie?.title ||
+              bannerMovie?.name ||
+              bannerMovie?.original_name}
+          </h1>
+          <div>
+            <button
+              className="mr-6 bg-white px-6 py-2 rounded-md text-2xl font-semibold"
+              onClick={() => {
+                setisClicked(true);
+              }}
+            >
+              play
+            </button>
+            <button className=" bg-gray-400 bg-opacity-55 px-4 py-2 rounded-md text-2xl font-semibold text-white">
+              More Information
+            </button>
+          </div>
+          <h1 className="text-white font-bold text-xl w-1/2">
+            {bannerMovie?.overview?.length > 100
+              ? bannerMovie?.overview?.substr(0, 99) + "..."
+              : bannerMovie?.overview}
+          </h1>
         </div>
-        <h1 className="text-white font-bold text-xl w-1/2">
-          {bannerMovie?.overview?.length > 100
-            ? bannerMovie?.overview?.substr(0, 99) + "..."
-            : bannerMovie?.overview}
-        </h1>
+      </header>
+    );
+  } else {
+    return (
+      <div className="h-screen w-screen bg-black">
+        <iframe
+          className="h-full w-screen opacity-55 pt-20 "
+          src={`https://www.youtube.com/embed/${bannerMovie.videos.results[0].key}?controls=0&autoplay=1&loop=1`}
+          allowFullScreen
+        ></iframe>
       </div>
-    </header>
-  );
+    );
+  }
 }
 
 export default Banner;
