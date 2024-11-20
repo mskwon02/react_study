6b2cf84a28e64562e63be96c35509e356849b6f4
mskwon02 <121784261+mskwon02@users.noreply.github.com>
2024-11-21 04:01:17 +0900

    Row컴포넌트 + 영화 이미지 클릭시 설명모달 나옴

---
+++ react-netflix-app/src/components/MovieModal.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+export default function MovieModal({ selectedMovie, setisClicked }) {
+  console.log("movie", selectedMovie);
+
+  return (
+    <div className="flex justify-center  fixed top-24 z-20 w-full h-full bg-black bg-opacity-60">
+      <div className="relative flex flex-col bg-black w-[60vw] h-[90vh] rounded-lg overflow-y-scroll no-scrollbar">
+        <div className="flex absolute right-5 top-5 justify-end">
+          <button
+            onClick={() => {
+              setisClicked(false);
+            }}
+            className="bg-white  rounded-full size-10 "
+          >
+            {"X"}
+          </button>
+        </div>
+        <div className=" w-full">
+          <img
+            src={`https://image.tmdb.org/t/p/original/${selectedMovie.backdrop_path} `}
+            className="object-cover w-full h-full "
+          />
+        </div>
+        <div className="flex flex-col p-8">
+          <div className="flex items-center mb-3 text-xl font-semibold">
+            <span className="pr-4  text-white">100% for you</span>
+            <span className=" pr-4  text-green-800">
+              {selectedMovie.first_air_date || selectedMovie.release_date}
+            </span>
+          </div>
+          <h1 className="font-extrabold text-3xl text-white">
+            {selectedMovie.name || selectedMovie.title}
+          </h1>
+          <p className="mt-7 text-white text-xl ">{`평점:${selectedMovie.vote_average}`}</p>
+          <p className="mt-4 text-white text-xl ">{selectedMovie.overview}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
--- react-netflix-app/src/components/Row.js
+++ react-netflix-app/src/components/Row.js
@@ -1,8 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import axios_instance from "../api/axios";
+import MovieModal from "./MovieModal";
 
 export default function Row({ title, id, rUrl, isBig }) {
   const [netfilxOriginalList, setnetfilxOriginalList] = useState([]);
+  const [isClicked, setisClicked] = useState(false);
+  const selectedMovie = useRef("");
   useEffect(() => {
     fetchData();
   }, []);
@@ -11,7 +14,7 @@ export default function Row({ title, id, rUrl, isBig }) {
     const result = await axios_instance.get(rUrl);
     setnetfilxOriginalList(result.data.results);
   };
-  console.log("netflix list", netfilxOriginalList);
+  //console.log("netflix list", netfilxOriginalList);
   return (
     <section className=" pt-16 bg-black">
       <h1 className="text-4xl font-semibold mb-4 ml-8 text-white">{title}</h1>
@@ -30,12 +33,19 @@ export default function Row({ title, id, rUrl, isBig }) {
         >
           {netfilxOriginalList.map((original) => {
             return (
-              <span className="flex-shrink-0 w-1/5 flex flex-col mx-3 ">
+              <span
+                key={original.id}
+                className="flex-shrink-0 w-1/5 flex flex-col mx-3 "
+              >
                 <img
                   src={`https://image.tmdb.org/t/p/original/${
                     isBig ? original.poster_path : original.backdrop_path
                   } `}
                   className="rounded-md hover:scale-[1.1] hover:rounded-md"
+                  onClick={() => {
+                    setisClicked(true);
+                    selectedMovie.current = original;
+                  }}
                 />
                 <span className="text-white font-medium mt-8 overflow-hidden text-xl">
                   {original.title || original.name}
@@ -53,6 +63,13 @@ export default function Row({ title, id, rUrl, isBig }) {
           <span>{">"}</span>
         </div>
       </div>
+
+      {isClicked && (
+        <MovieModal
+          selectedMovie={selectedMovie.current}
+          setisClicked={setisClicked}
+        />
+      )}
     </section>
   );
 }
